03ae9022e27815d69e3fa83d1d9d9cda21dceead
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-07-22 22:15:53 +0000

    Add fake data to cli.rb

--- lib/ny_state_fair_concerts_cli_gem/cli.rb
+++ lib/ny_state_fair_concerts_cli_gem/cli.rb
@@ -3,7 +3,23 @@
 class NyStateFairConcerts::CLI
 
   def call
-    puts "2018 Chevy Court Concerts"
+    list_concerts
+    menu
+  end
+
+  def list_concerts
+    puts "2018 Chevy Court Concerts:"
+    puts <<-DOC.gsub /^\s*/, ''
+      1. Chevy Court TBA - Aug. 22 @ 2pm
+      2. Blondie
+      3. Dave Mason and Steve Cropper
+      4. Chevy Court TBA - Aug. 23 @ 8pm
+      5. Ravyn Lenae
+    DOC
+  end
+
+  def menu
+    puts "Enter the number of the concert you'd like more information on:"
   end
 
 end
