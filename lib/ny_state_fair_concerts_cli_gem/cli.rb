e83d3c819fc241ff673e0bdbf0463aac542a0c38
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-07-22 23:02:37 +0000

    Add fake data to cli.rb

--- lib/ny_state_fair_concerts_cli_gem/cli.rb
+++ lib/ny_state_fair_concerts_cli_gem/cli.rb
@@ -5,6 +5,7 @@ class NyStateFairConcerts::CLI
   def call
     list_concerts
     menu
+    goodbye
   end
 
   def list_concerts
@@ -13,13 +14,35 @@ class NyStateFairConcerts::CLI
       1. Chevy Court TBA - Aug. 22 @ 2pm
       2. Blondie
       3. Dave Mason and Steve Cropper
-      4. Chevy Court TBA - Aug. 23 @ 8pm
-      5. Ravyn Lenae
     DOC
   end
 
   def menu
-    puts "Enter the number of the concert you'd like more information on:"
+    input = ''
+    while input != 'exit'
+      puts <<-DOC.gsub /^\s*/, ''
+        Enter one of the following prompts:
+        - Concert number to get more information on a specific concert
+        - Concerts to list all Concerts
+        - Exit
+      DOC
+      input = gets.strip.downcase
+      if input == "1"
+        puts "date, time, and url for concert 1"
+      elsif input == "2"
+        puts "date, time, and url for concert 2"
+      elsif input == "3"
+        puts "date, time, and url for concert 3"
+      elsif input == 'concerts'
+        list_concerts
+      else
+        puts "I'm sorry, I didn't catch that."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Hope to see you at Chevy Court this year!"
   end
 
 end
