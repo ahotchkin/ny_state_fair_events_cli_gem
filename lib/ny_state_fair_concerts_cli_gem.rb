950f76fe8e629aaffad36333083ea4d1e91fff20
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-07-25 00:32:05 +0000

    Require OpenURL, Nokogiri, and Pry in ny_state_fair_concerts_cli_gem.rb

--- lib/ny_state_fair_concerts_cli_gem.rb
+++ lib/ny_state_fair_concerts_cli_gem.rb
@@ -1,3 +1,8 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+# Added this here based on Avi's CLI Gem Walkthrough. Need to move or remove? 
+
 require_relative './ny_state_fair_concerts_cli_gem/version'
 require_relative './ny_state_fair_concerts_cli_gem/concerts'
 require_relative './ny_state_fair_concerts_cli_gem/cli'
