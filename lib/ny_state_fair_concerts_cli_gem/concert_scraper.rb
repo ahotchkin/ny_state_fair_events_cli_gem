136cc986051df6b4b03a476fcba32dcfe1983e97
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-07-26 23:42:10 +0000

    Add #make_concerts and create spec file for concert_scraper.rb

--- lib/ny_state_fair_concerts_cli_gem/concert_scraper.rb
+++ lib/ny_state_fair_concerts_cli_gem/concert_scraper.rb
@@ -1,14 +1,12 @@
 class NYStateFairConcerts::ConcertScraper
 
-  @@concerts = {}
+# What is the best way to have the the ConcertScraper class interact with the Concert class? Should one class inherit from other, or should one class require the other's file?
+  require_relative './concerts'
+
+  @@concerts = []
   def scrape_concerts
     document = Nokogiri::HTML(open('https://nysfair.ny.gov/venue/chevy-court/'))
     concerts = document.css('tr.event-list-feed-item td.eventcol')
-    band = concerts.css('td.eventcol')[i].text
-    date = concerts.css('td.datecol')[i].text.gsub(/[\t\n]/, '')
-    time = concerts.css('td.timecol')[i].text
-    links = concerts.css('a').map { |link| link.attribute('href').value }
-    link = links[i]
 
     # Concerts = document.css('tr.event-list-feed-item td.eventcol')
     # Band: concerts.css('td.eventcol')[i].text
@@ -16,6 +14,19 @@ class NYStateFairConcerts::ConcertScraper
     # Time: concerts.css('td.timecol')[i].text
     # Link: links[i]
     # links = concerts.css('a').map { |link| link.attribute('href').value }
+  end
+
+  def make_concerts
+    self.scrape_concerts.each do |concert|
+      binding.pry
+      concert = Concert.new
+      concert.band = concert.css('td.eventcol').text
+      concert.date = concert.css('td.datecol').text.gsub(/[\t\n]/, '')
+      concert.time = concert.css('td.timecol').text
+      concert.links = concert.css('a').map { |link| link.attribute('href').value }
+      @@concerts << concert
+    end
+    @@concerts
   end
 
 end
