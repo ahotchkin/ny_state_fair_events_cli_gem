77a8893c4e51e2988c58f519b617c0d80601fc8b
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-08-05 16:46:43 +0000

    Update CLI asthetics

--- lib/ny_state_fair_concerts/cli.rb
+++ lib/ny_state_fair_concerts/cli.rb
@@ -4,8 +4,39 @@ class NYStateFairConcerts::CLI
 
   def call
     NYStateFairConcerts::ConcertScraper.new.make_concerts
+    puts ""
     puts "Welcome to the 2018 New York State Fair Concerts app!"
     menu
+  end
+
+  def menu
+    input = ""
+    while input != "exit"
+      puts ""
+      puts <<-DOC.gsub /^\s*/, ""
+        What would you like to do?
+        - Enter 'concerts' to see a list of all concerts
+        - Enter 'help' for instructions
+        - Enter 'directions' for directions to Chevy Court
+        - Enter 'exit' to exit
+      DOC
+      puts ""
+      input = gets.strip.downcase
+
+      if input == "concerts"
+        list_concerts
+      elsif input == "help"
+        puts ""
+        puts "This app is designed to provide information on all concerts taking place at Chevy Court during the 2018 New York State Fair. Follow the prompts for concert details."
+      elsif input == "directions"
+        directions
+      elsif input != "exit"
+        puts ""
+        puts "I'm sorry, I didn't catch that."
+      elsif input == "exit"
+        goodbye
+      end
+    end
   end
 
   def list_concerts
@@ -20,7 +51,11 @@ class NYStateFairConcerts::CLI
     input = ""
     while input != "exit"
       puts ""
-      puts "Enter a concert number to get details on that concert. Otherwise, enter 'menu'"
+      puts <<-DOC.gsub /^\s*/, ""
+        Enter a concert number to get details on that concert. Otherwise, enter one of the following prompts:
+        - Enter 'menu' to see the main menu
+        - Enter 'exit' to exit
+      DOC
       puts ""
       input = gets.strip.downcase
 
@@ -39,42 +74,17 @@ class NYStateFairConcerts::CLI
       elsif input != "exit"
         puts ""
         puts "I'm sorry, I didn't catch that."
-        puts ""
       elsif input == "exit"
         goodbye
       end
     end
   end
 
-  def menu
-    input = ""
-    while input != "exit"
-      puts <<-DOC.gsub /^\s*/, ""
-        What would you like to do?
-        - Enter 'concerts' to see a list of all concerts
-        - Enter 'help' for instructions
-        - Enter 'directions' for directions to Chevy Court
-        - Enter 'exit' to exit
-      DOC
-      input = gets.strip.downcase
-
-      if input == "concerts"
-        list_concerts
-      elsif input == "help"
-        puts ""
-        puts "This app is designed to provide information on all concerts taking place at Chevy Court during the 2018 New York State Fair. Follow the prompts for concert details."
-        puts ""
-      elsif input == "directions"
-        puts "Click on the below link for directions to Chevy Court:"
-        puts "https://www.google.com/maps/place/43.0720149,-76.2153672"
-      elsif input != "exit"
-        puts ""
-        puts "I'm sorry, I didn't catch that."
-        puts ""
-      elsif input == "exit"
-        goodbye
-      end
-    end
+  def directions
+    puts ""
+    puts "Click on the below link for directions to Chevy Court:"
+    puts "https://www.google.com/maps/place/43.0720149,-76.2153672"
+    puts ""
   end
 
   def goodbye
