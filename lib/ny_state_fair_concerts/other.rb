6abe6be671b1be15caf413db821b79192ae927bd
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-08-12 17:49:38 +0000

    Update #other_details to display list of dates

--- lib/ny_state_fair_concerts/other.rb
+++ lib/ny_state_fair_concerts/other.rb
@@ -19,20 +19,23 @@ class NYStateFairConcerts::Other
 
   def self.list_all
     names = @@all.map { |other| other.name }.uniq
-    names.each_with_index do |other, i|
-      puts "#{i + 1}. #{other}"
+    names.each.with_index(1) do |other, i|
+      puts "#{i}. #{other}"
     end
   end
 
   def other_details
 
-    line = "-" * (self.date.length) + "-" * 30
+    line = "-" * (self.date.length) + "-" * 20
     top_dashes = "-" * (((line.length / 2).to_f) - ((self.name.length / 2).to_f)).abs
     bottom_dashes = ("-" * (top_dashes.length * 2)) + ("-" * self.name.length)
 
     puts top_dashes + "#{self.name.upcase}" + top_dashes
+    puts ""
     @@all.each.with_index(1) do |other, i|
-      puts "#{i}. #{other.date} at #{other.time}"
+      if other.name == self.name
+        puts "#{other.date} at #{other.time}"
+      end
     end
     puts ""
     puts "For more information visit: #{self.url}"
